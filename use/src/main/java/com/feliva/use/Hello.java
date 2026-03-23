0ed166681cf82b540a9ef573657146da2cc11160
darlan <darlanfelisberto@gmail.com>
2026-03-23 17:46:15 -0300

    aa

---
+++ use/src/main/java/com/feliva/use/Hello.java
@@ -0,0 +1,30 @@
+package com.feliva.use;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class Hello {
+
+    private String name;
+    private String message;
+
+    public void createMessage() {
+        message = "Hello, " + name + "!";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+
+    }
+
+}
